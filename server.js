bc3fb122c1e5095aef2ddf43fc6c9e2472bb71d8
Ahmed Bakheet <ahmed.said.bakheet@gmail.com>
2020-06-09 21:29:47 +0200

    inint backend project, init server file and connect it to an empty mongodb cluster

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const connectDB =require('./config/db')
+const app = express();
+connectDB()
+app.get('/',(req,res)=>{res.send('API running')})
+const port =process.env.PORT || 5000
+app.listen(port,()=>{console.log(`server started in port ${port}`)})
